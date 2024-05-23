709d9f5fcf3ab79786fd1fb38ffad40f1e987fc3
Marco Ciau <marcoantoniociau@hotmail.com>
2024-05-22 18:21:57 -0600

    fixes: add location and vbat for sensors data, also build packet for telemetry position message

--- MCU_Comm/LoRaWANBridge/lorawan_comm_bridge.c
+++ MCU_Comm/LoRaWANBridge/lorawan_comm_bridge.c
@@ -54,19 +54,54 @@ uint16_t lorawan_comm_buildTelemetrySensorsPacket(uint16_t sequence, telemetry_s
     payloadPtr += TELEMETRY_HEADER_SIZE;
     splitUint32(payload.timestamp, payloadPtr);
     payloadPtr += 4;
-
     splitFloat(payload.temperature, payloadPtr);
     payloadPtr += 4;
-
     splitFloat(payload.flow, payloadPtr);
     payloadPtr += 4;
-
     splitFloat(payload.consumption, payloadPtr);
     payloadPtr += 4;
-
     splitFloat(payload.pressure, payloadPtr);
     payloadPtr += 4;
-
     splitFloat(payload.humidity, payloadPtr);
-	return ((6 * 4) + MAIN_HEADER_SIZE + TELEMETRY_HEADER_SIZE);
+    payloadPtr += 4;
+    splitFloat(payload.vBatLevel, payloadPtr);
+    #ifdef ADD_GPS_TO_SENSORS
+    payloadPtr += 1;
+    splitUint32(payload.latitude, payloadPtr);
+    payloadPtr += 4;
+    splitUint32(payload.longitude, payloadPtr);
+    payloadPtr += 4;
+    splitUint32(payload.altitude, payloadPtr);
+    #endif
+	return (MAIN_HEADER_SIZE + TELEMETRY_HEADER_SIZE + mainHeader.size);
+}
+
+uint16_t lorawan_comm_buildTelemetryPositionPacket(uint16_t sequence, mcu_comm_encoder_position_t payload, uint8_t* outputBuff)
+{
+    lorawan_comm_packet_main_header_t mainHeader;
+	lorawan_comm_packet_telemetry_header_t telemetryHeader;
+	mainHeader.type = (uint16_t)MCU_COMM_TYPE_TELE_POSITION;
+	mainHeader.size = sizeof(telemetry_sensor_data_t);
+	telemetryHeader.sequence = sequence;
+	// Fill buffer with headers
+    uint8_t* payloadPtr = outputBuff;
+	lorawan_comm_buildCommonHeader(&mainHeader, payloadPtr);
+    payloadPtr += MAIN_HEADER_SIZE;
+    lorawan_comm_buildTelemetryHeader(&telemetryHeader, outputBuff + MAIN_HEADER_SIZE);
+    payloadPtr += TELEMETRY_HEADER_SIZE;
+    // Append telemetry position data to the buffer
+    payloadPtr[0] = payload.fix;
+    payloadPtr[1] = payload.satellites;
+    payloadPtr[2] = payload.day;
+    payloadPtr[3] = payload.month;
+    payloadPtr[4] = payload.year;
+    payloadPtr += 5;
+    splitUint32(payload.time, payloadPtr);
+    payloadPtr += 4;
+    splitUint32(payload.latitude, payloadPtr);
+    payloadPtr += 4;
+    splitUint32(payload.longitude, payloadPtr);
+    payloadPtr += 4;
+    splitUint32(payload.altitude, payloadPtr);
+    return (MAIN_HEADER_SIZE + TELEMETRY_HEADER_SIZE + mainHeader.size);
 }
--- MCU_Comm/LoRaWANBridge/lorawan_comm_bridge.h
+++ MCU_Comm/LoRaWANBridge/lorawan_comm_bridge.h
@@ -8,15 +8,23 @@
 #ifndef LORRAWANBRIDGE_LORAWAN_COMM_BRIDGE_H_
 #define LORRAWANBRIDGE_LORAWAN_COMM_BRIDGE_H_
 #include <stdint.h>
+#include "mcu_comm_encoder.h"
 #define MAIN_HEADER_SIZE 4
 #define TELEMETRY_HEADER_SIZE 2
-typedef struct {
+#define ADD_GPS_TO_SENSORS 1
+typedef struct __attribute__((packed)) {
     uint32_t timestamp;
     float temperature;
     float flow;
     float consumption;
     float pressure;
     float humidity;
+    uint8_t vBatLevel;
+    #ifdef ADD_GPS_TO_SENSORS
+    int32_t latitude;       
+    int32_t longitude;     
+    int16_t altitude;    
+    #endif
 } telemetry_sensor_data_t;
 
 typedef struct
@@ -33,5 +41,6 @@ typedef struct
 uint16_t lorawan_comm_buildCommonHeader(lorawan_comm_packet_main_header_t* headerData, uint8_t * outputBuff);
 uint16_t lorawan_comm_buildTelemetryHeader(lorawan_comm_packet_telemetry_header_t* headerData, uint8_t * outputBuff);
 uint16_t lorawan_comm_buildTelemetrySensorsPacket(uint16_t sequence, telemetry_sensor_data_t payload, uint8_t* outputBuff);
+uint16_t lorawan_comm_buildTelemetryPositionPacket(uint16_t sequence, mcu_comm_encoder_position_t payload, uint8_t* outputBuff);
 
 #endif /* LORRAWANBRIDGE_LORAWAN_COMM_BRIDGE_H_ */
